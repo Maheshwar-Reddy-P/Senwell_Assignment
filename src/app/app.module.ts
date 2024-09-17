013f50882522a2e2bb62a2ae9f2846d47c21a38d
sharan <sharanambati26@gmail.com>
2024-09-17 16:16:59 +0530

    Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayTableComponent } from './display-table/display-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayTableComponent
   ],
   imports: [
     BrowserModule,
